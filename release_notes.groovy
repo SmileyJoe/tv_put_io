238051e27487f074cb8370b4b878da5d28522f27
Cody Strugnell <cody.strugnell@gmail.com>
2022-05-09 22:17:01 +0200

    Seperating store and dev release notes

--- release_notes.groovy
+++ release_notes.groovy
@@ -4,11 +4,17 @@ ext.generateReleaseNotes = {flavour ->
     def notesFile = new File(projectDir.getParentFile(), 'release_notes.json')
     def json = new JsonSlurper().parseText(notesFile.text)
     def version, feature, bug
+    def type
 
-    if(flavour.equalsIgnoreCase("debug")){
+    if(flavour.equalsIgnoreCase("store")){
+        (version, feature, bug) = json.get("previous_release").tokenize('.')
+        type = "store";
+    } else if(flavour.equalsIgnoreCase("debug")){
         (version, feature, bug) = json.get("current_debug").tokenize('.')
+        type = "developer";
     } else {
         (version, feature, bug) = json.get("previous_release").tokenize('.')
+        type = "developer";
         bug++
     }
 
@@ -20,7 +26,7 @@ ext.generateReleaseNotes = {flavour ->
         while(notes_version.keySet().contains(feature)){
             def notes_feature = notes_version.get(feature)
             while(notes_feature.keySet().contains(bug)){
-                notes_feature.get(bug).each{note -> release_notes += " - " + note + "\n"}
+                notes_feature.get(bug).get(type).each{note -> release_notes += " - " + note + "\n"}
                 bug++
             }
             bug = "0"
@@ -33,13 +39,13 @@ ext.generateReleaseNotes = {flavour ->
     return release_notes
 }
 
-task generateReleaseNotesRelease { task ->
+task generateReleaseNotesStore { task ->
     doLast {
         def fileReleaseNotes = new File(projectDir,'src/main/play/release-notes/en-GB/default.txt')
         fileReleaseNotes.getParentFile().mkdirs()
         if(!fileReleaseNotes.exists()){
             fileReleaseNotes.createNewFile()
         }
-        fileReleaseNotes.write generateReleaseNotes("release")
+        fileReleaseNotes.write generateReleaseNotes("store")
     }
 }
